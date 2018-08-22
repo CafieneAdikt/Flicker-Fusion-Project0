95ec361818a31ca4cb5b7398c004fb519f3ada0f
CafieneAdikt <tearacepeterson13@gmail.com>
2018-08-22 14:39:29 +1200

    fixed maths bug

--- src/main.cpp
+++ src/main.cpp
@@ -2,62 +2,65 @@
 
 // declare variables
 
- byte ADpin = 14;   // pin assigned to variable
- int AD0 = 0;       // raw A/D converter value
- unsigned long interval = 0;  // mapped analog input
- long intervalInseconds = 0; 
- int LED = 6;       // assign pin 6 to led
- const int INTBUTTON = 16; // interupt button
- bool Buttonpressed = false; // flag for button
- const unsigned long DEBOUNCE_TIME_MS = 100;  
- unsigned long previousdebounce = 0;
- unsigned long previousmillis = 0; // will store last time led was updated
- long frequency = 0; // frequency of the led
+byte ADpin = 14;            // pin assigned to variable
+int AD0 = 0;                // raw A/D converter value
+unsigned long interval = 0; // mapped analog input
+float intervalInSeconds = 0;
+int LED = 6;                         // assign pin 6 to led
+const int INTBUTTON = 16;            // interupt button
+volatile bool buttonPressed = false; // flag for button
+const unsigned long DEBOUNCE_TIME_MS = 200;
+unsigned long previousDebounce = 0;
+unsigned long frequency = 0; // frequency of the led
 
-  void Buttonpressed_ISR(){
-      // send flag high
-      Buttonpressed = true;
-  
-}
- 
-
+void Buttonpressed_ISR();
 
 void setup() {
 
-    Serial.begin(115200); // set baud rate
-    pinMode(ADpin,INPUT);
-    pinMode(LED,OUTPUT);
-    pinMode(INTBUTTON,INPUT_PULLUP); // interupt button
-    attachInterrupt(digitalPinToInterrupt(INTBUTTON),Buttonpressed_ISR,FALLING);
-
+  Serial.begin(115200); // set baud rate
+  pinMode(ADpin, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(INTBUTTON, INPUT_PULLUP); // interupt button
+  attachInterrupt(digitalPinToInterrupt(INTBUTTON), Buttonpressed_ISR, FALLING);
 }
 
 void loop() {
-    // variable that stores current time
-    unsigned long currentmillis = millis();
 
-    AD0 = analogRead(ADpin);    // read input from potentiometer
+  // will store last time led was updated in the loop
+  static unsigned long previousmillis = 0;
 
-    interval = map(AD0, 0, 1023, 50, 1); // map 10bit number to a interval to cause flicker fusion
+  // variable that stores current time
+  unsigned long currentmillis = millis();
 
-    if(currentmillis - previousmillis >= interval){
-        //save last time it blinks
-        previousmillis = currentmillis;
+  AD0 = analogRead(ADpin); // read input from potentiometer
 
-        // change the state of the led to opposite of previous state 
+  interval = map(AD0, 0, 1023, 50,
+                 1); // map 10bit number to a interval to cause flicker fusion
+
+  if (currentmillis - previousmillis >= interval) {
+    // save last time it blinks
+    previousmillis = currentmillis;
+
+    // change the state of the led to opposite of previous state
     digitalWrite(LED, !digitalRead(LED));
-    }
+  }
 
-    intervalInseconds = interval/1000;
+  intervalInSeconds = interval / 1000.0;
 
-    frequency = 1/(2*intervalInseconds);
-
-    if(Buttonpressed && ((currentmillis-previousdebounce) >= DEBOUNCE_TIME_MS)){
-        previousdebounce = currentmillis; // set new debounce time
-        Serial.println(String(frequency) + "Hz");
-        // reset flag
-        Buttonpressed = false; 
-    }
-
+  frequency = 1 / (2 * intervalInSeconds);
 }
 
+void Buttonpressed_ISR() {
+  // send flag high
+  buttonPressed = true;
+
+  unsigned long currentmillis = millis();
+
+  if (buttonPressed &&
+      ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS)) {
+    previousDebounce = currentmillis; // set new debounce time
+    Serial.println("Flicker Fusion Thresthold Frequency :  " + String(frequency) + "Hz");
+    // reset flag
+    buttonPressed = false;
+  }
+}
